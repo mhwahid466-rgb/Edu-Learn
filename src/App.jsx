1b702662a6eb9c7ea698f1dbc40879681e75e35d
mhwahid466-rgb <https://github.com/mhwahid466-rgb/>
2026-07-26 00:12:20 +0500

    Fix Signup import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
-import Signup from "./pages/SignUp.jsx";
+import  Signup from "./pages/Signup.jsx";
 import Signin from "./pages/Signin.jsx";
 import Home from "./pages/Home.jsx";
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Routes>     
             <Route path="/" element={<Home/>} />
-            <Route path="/SignUp" element={<Signup />} />
+            <Route path="/SignUp" element={< Signup />} />
        <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
